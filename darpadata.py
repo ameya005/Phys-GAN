6fcb03b823252fa6e5fb1f3702183d4be02a316d
ameya005 <ameya@iastate.edu>
2019-08-06 14:08:57 -0500

    darpa

---
+++ darpadata.py
@@ -0,0 +1,50 @@
+"""
+Matsci data loader
+"""
+
+import torch
+import h5py
+import numpy as np
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset
+
+
+class DarpaDataset(Dataset):
+    """
+    Class to read the h5 dataset for the p1 values
+    """
+
+    def __init__(self, data_path, transform=None, train='train'):
+        super(DarpaDataset, self).__init__()
+        #self.data = h5py.File(data_path, mode='r')['dataset']
+        self.data = np.load(data_path)
+        self.data = np.array(self.data) - 1                 # Forcing to have label 0 to 5.
+        self.data = self.data.astype(int)                   # every element should be int.
+        if train == 'train':
+            self.data = self.data[:9, ...]
+        elif train == 'valid':
+            self.data = self.data[9:, ...]
+        else:
+            self.data = self.data[9:,...]
+        self.data = torch.from_numpy(self.data)             # converting to tensor
+        self.data = F.one_hot(self.data, num_classes=6)                    # one_hot
+        self.data = self.data.transpose(1, 3)
+        self.transform = transform
+
+    def __getitem__(self, index):
+        # x = torch.FloatTensor(self.data[index, ...])
+        x = self.data[index, ...]
+        if self.transform is not None:
+            x = self.transform(x)
+        # print(x.min(), x.max())
+        # p1 = torch.FloatTensor(x.mean())
+        return x
+
+    def __len__(self):
+        return self.data.shape[0]
+
+# data_path = './datasets/voronoi/orient_voronoi_noZero.h5'
+# v = VoronoiDataset(data_path=data_path)
+#
+# print(v[0])
